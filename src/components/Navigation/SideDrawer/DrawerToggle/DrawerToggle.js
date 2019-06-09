85fe2960a14a9fa62a9e1112c406d6da11de277c
grzeli <grzelak.szymon@gmail.com>
2019-06-09 21:49:22 +0200

    backdrop&hamburger icon for sidedrawer

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './DrawerToggle.css';
+
+const drawerToogle = (props) => (
+    <div
+        className={classes.DrawerToggle}
+        onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToogle;
